2784bbec6c4330b9c4eaf66a2ca14e167958264f
kathy <katherinechan4@gmail.com>
2023-01-31 01:02:22 -0500

    move docstrings inside functions

--- src/canvas.py
+++ src/canvas.py
@@ -1,20 +1,20 @@
 # Python GUI library
 import tkinter as tk 
 
-""" 
-Draw on the canvas with the mouse.
-MouseDown (start of input), MouseDragged (input continues), and MouseUp (input ends).
-"""
 def paint(event):
+    """ 
+    Draw on the canvas with the mouse.
+    MouseDown (start of input), MouseDragged (input continues), and MouseUp (input ends).
+    """
     x1, y1 = (event.x - 1.5), (event.y - 1.5)
     x2, y2 = (event.x + 1.5), (event.y + 1.5)
     print("Mousedown: " + str(event.x) + "," + str(event.y))
     canvas.create_oval(x1, y1, x2, y2, fill="black")
 
-"""
-Clear the canvas.
-"""
 def clear_canvas():
+    """
+    Clear the canvas.
+    """
     canvas.delete("all")
 
 # ROOT WINDOW
