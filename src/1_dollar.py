6ff150463548f2d5c0d266e21c5f284c9480cd43
kathy <katherinechan4@gmail.com>
2023-02-16 14:59:52 -0500

    lint: fix with black linting

--- src/1_dollar.py
+++ src/1_dollar.py
@@ -5,7 +5,7 @@ import stored_gestures
 from collections import defaultdict
 import random
 
-live = False # True = online mode, False = offline mode.
+live = False  # True = online mode, False = offline mode.
 
 if __name__ == "__main__":
     if live:
@@ -28,10 +28,10 @@ if __name__ == "__main__":
             "left curly brace",
             "right curly brace",
             "star",
-            "pigtail"
+            "pigtail",
         ]
-        
-        print("Starting loop.") # Loop over data set
+
+        print("Starting loop.")  # Loop over data set
 
         ### PSEUDOCODE
         # for each user U = 1 to 10
@@ -53,23 +53,29 @@ if __name__ == "__main__":
                 # user recognition score for each example E
                 user_recognition_score = 0
                 for i in range(100):
-                    recognizer = DollarRecognizer(points=[], live=False) # Initialize recognizer in offline mode.
+                    recognizer = DollarRecognizer(
+                        points=[], live=False
+                    )  # Initialize recognizer in offline mode.
                     templates = defaultdict(list)
                     # key: gesture_name
-                        # value: [template_1, template_2, ...]
+                    # value: [template_1, template_2, ...]
                     candidates = defaultdict(list)
                     # key: gesture_name
-                        # value: candidate_points
-                    
-                    for gesture in gestures:              
+                    # value: candidate_points
+
+                    for gesture in gestures:
                         for j in range(1, E + 1):
                             # choose E templates from U,G set
                             possible_template = [i for i in range(10)]
-                            this_template = possible_template.pop(random.randint(0, len(possible_template) - 1))
+                            this_template = possible_template.pop(
+                                random.randint(0, len(possible_template) - 1)
+                            )
                             templates[gesture].append(this_template)
 
                             # choose 1 candidate from remaining templates
-                            this_candidate = possible_template.pop(random.randint(0, len(possible_template) - 1))
+                            this_candidate = possible_template.pop(
+                                random.randint(0, len(possible_template) - 1)
+                            )
                             candidates[gesture] = this_candidate
                     for candidate_name, candidate_points in candidates.items():
                         # TODO: process all templates stored in templates for each gesture
@@ -83,7 +89,9 @@ if __name__ == "__main__":
                     # add recognition score for each user, gesture to user_recognition_scores
                     user_recognition_scores.append(user_recognition_score)
                 # recognition score for each U,G /= 100
-                user_recognition_scores = [score / 100 for score in user_recognition_scores]
+                user_recognition_scores = [
+                    score / 100 for score in user_recognition_scores
+                ]
         # print final average per-user accuracy
         for score in user_recognition_scores:
             print(score)
