f81d24f71944046b935cc59625ab7a1f864b894e
jonathanzhang53 <jonathanzhang53@gmail.com>
2023-02-15 22:17:39 -0500

    loop work

--- src/1_dollar.py
+++ src/1_dollar.py
@@ -1,44 +1,84 @@
 from canvas import Canvas
 from recognizer import DollarRecognizer
-from collections import defaultdict
-import random
 import stored_gestures
 
+from collections import defaultdict
+import random
+
 live = False # True = online mode, False = offline mode.
+
 if __name__ == "__main__":
     if live:
         canvas = Canvas()
         canvas.run()
     else:
-        recognizer = DollarRecognizer([], False) # Initialize recognizer in offline mode.
-    
+        gestures = [
+            "triangle",
+            "x",
+            "rectangle",
+            "circle",
+            "check",
+            "caret",
+            "zig-zag",
+            "arrow",
+            "left square bracket",
+            "right square bracket",
+            "v",
+            "delete",
+            "left curly brace",
+            "right curly brace",
+            "star",
+            "pigtail"
+        ]
+        
         print("Starting loop.") # Loop over data set
-        gestures = ["triangle", "x", "rectangle", "circle", "check", "caret", "zig-zag", "arrow", "left square bracket", "right square bracket", "v", "delete", "left curly brace", "right curly brace", "star", "pigtail"]
+
+        ### PSEUDOCODE
+        # for each user U = 1 to 10
+        #   for each example E = 1 to 9
+        #       for i = 1 to 100
+        #           for each gesture G in gestures
+        #               choose E templates from U,G set
+        #               choose 1 candidate from remaining templates
+        #           for each candidate T from 1 to G
+        #               recognize candidate with E chosen templates
+        #               if recognition correct
+        #                   increment recognition score for each U,G by 1
+        #       recognition score for each U,G /= 100
+        # print final average per-user accuracy
+        
+        # loop through each user, gathering recognition scores for 100 tests each involving 1 to 9 templates (900 tests x 16 gestures per user)
         for user in range(2, 12):
             for E in range(1, 10):
                 for i in range(100):
-                    candidates = defaultdict(list)
-
+                    recognizer = DollarRecognizer([], False) # Initialize recognizer in offline mode.
+                    templates = defaultdict(list)
                     # key: gesture_name
-                        # value: [[points for template_1], [points for template_2]]
-                    training_set = map()
+                        # value: [template_1, template_2, ...]
+                    candidates = defaultdict(list)
+                    # key: gesture_name
+                        # value: candidate_points
+                    
                     for gesture in gestures:              
-                        # choose E templates
-                        possible_instances = [i for i in range(10)]
-                        for j in range(1,E+1): 
-                            for gesture_2 in gestures:
-                                # pick a random instance and take it out of the list
-                                this_instance = possible_instances.pop(random.randint(0, len(possible_instances)-1))
-                                training_set[f"{gesture}{E}"] = stored_gestures.preprocessed_dataset[user][this_instance][gesture_2]
-                            
-                        this_instance = possible_instances.pop(random.randint(0, len(possible_instances)-1))
-                        candidate = stored_gestures.preprocessed_dataset[user][this_instance][gesture]
-                        # choose 1 candidate from remaining templates
-                        # candidates["template_name"] = template_points
-                    for candidate_name, candidate_points in candidates.items():
-                        # recognize candidate with E chosen templates
-                        #TODO: insert points
-                        recognizer.points = candidate_points
-                        print(recognizer.run())             
-                        
+                        for j in range(1, E + 1):
+                            # choose E templates from U,G set
+                            possible_template = [i for i in range(10)]
+                            this_template = possible_template.pop(random.randint(0, len(possible_template) - 1))
+                            templates[gesture].append(this_template)
 
+                            # choose 1 candidate from remaining templates
+                            this_candidate = possible_template.pop(random.randint(0, len(possible_template) - 1))
+                            candidates[gesture] = this_candidate
+                    for candidate_name, candidate_points in candidates.items():
+                        # TODO: process all templates stored in templates for each gesture
+
+                        # recognize candidate with E chosen templates
+                        recognizer.points = candidate_points
+                        gesture_name, score = recognizer.run()
+
+                        if gesture_name == candidate_name:
+                            # TODO: increment recognition score for each U,G by 1
+                            pass
+                
+                # TODO: recognition score for each U,G /= 100
+        # TODO: print final average per-user accuracy
