70e542acce984ee86b83808bc26baee16e7d20a0
Thomas-Ruby <thomasruby@ufl.edu>
2023-02-24 20:11:29 -0500

    adds gesture prompts

--- src/canvas.py
+++ src/canvas.py
@@ -20,15 +20,21 @@ class Canvas:
         # contains the raw points for the user's currently drawn gesture
         self.raw_input_points = []
 
+        if gather_mode:
+            self.all_gesture_names = ["triangle", "x", "rectangle", "circle", "check", "caret", "zig-zag", "arrow", "left_square_bracket", "right_square_bracket", "v", "delete", "left_curly_brace", "right_curly_brace", "star", "pigtail"]
+
         # GUI setup
         self.root = tk.Tk()
-        self.text = tk.Text(self.root, height=1, width=40)
         self.canvas = tk.Canvas(self.root, width=300, height=200)
         self.gather_mode = gather_mode
         if self.gather_mode:
+            self.text = tk.Text(self.root, height=1, width=40, background="light grey")
+            self.text2 = tk.Text(self.root, height=2, width=40, background="light grey")
             self.gathered_gestures = defaultdict(list)
             self.sample_count = 1
-            self.current_gesture_name = ""
+            self.current_gesture_name = self.all_gesture_names[0]
+        else:
+            self.text = tk.Text(self.root, height=1, width=40)
 
         # initialize DollarRecognizer
         self.dollar_recognizer = DollarRecognizer(self.raw_input_points, True)
@@ -43,7 +49,9 @@ class Canvas:
         # TEXT
         self.text.pack()
         if self.gather_mode:
+            self.text2.pack()
             self.text.insert("1.0", "Sample " + str(self.sample_count) + " of 160")
+            self.text2.insert("1.0", "Please draw a: " + str.upper(self.current_gesture_name))
 
         # CANVAS
         self.canvas.config(bg="gray")
@@ -65,12 +73,12 @@ class Canvas:
 
     def on_mouseup_recognize(self, event):
         """
-        When the mouse is released, run the recognizer and display the result
+        When the mouse is released (in ONLINE mode), run the recognizer and display the result.
         """
         self.last_x = None
         self.last_y = None
-        self.dollar_recognizer.points = self.raw_input_points
         if not self.gather_mode:
+            self.dollar_recognizer.points = self.raw_input_points
             name_of_gesture, gesture_score, N_best_list = (self.dollar_recognizer.run())
             self.text.delete("1.0", "end")
             self.text.insert("1.0", "Result: " + name_of_gesture + ", Score: " + str(gesture_score)[0:5])
@@ -84,11 +92,20 @@ class Canvas:
 
         # increment sample count
         self.sample_count += 1
-        self.text.delete("1.0", "end")
-        self.text.insert("1.0", "Sample " + str(self.sample_count) + " of 160")
+
 
         # TODO: save to XML file
-        print(self.raw_input_points)
+        #print(self.raw_input_points)
+
+        # update current gesture name
+        # TODO: make this random (?)
+        self.current_gesture_name = self.all_gesture_names[(self.sample_count)%self.all_gesture_names.__len__()]
+
+        # update text
+        self.text.delete("1.0", "end")
+        self.text2.delete("1.0", "end")
+        self.text.insert("1.0", "Sample " + str(self.sample_count) + " of 160")
+        self.text2.insert("1.0", "Please draw a: " + str.upper(self.current_gesture_name))
 
         # clear canvas for next sample
         self.clear_canvas()
