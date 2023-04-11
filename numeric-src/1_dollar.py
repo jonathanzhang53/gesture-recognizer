e957832a4613f46bc55e50a6af1c82c46118031c
jonathanzhang53 <jonathanzhang53@gmail.com>
2023-04-11 16:48:56 -0400

    1 dollar

--- numeric-src/1_dollar.py
+++ numeric-src/1_dollar.py
@@ -12,89 +12,35 @@ from canvas import Canvas
 from recognizer import DollarRecognizer
 import stored_gestures
 
-
-def str2bool(v: str) -> bool:
-    return v.lower() in ("yes", "true", "t", "1")
-
-
-# python3 1_dollar.py [live: boolean ("yes", "true", "t", "1")] [offline_i: int] [num_users: int] [dataset: string ("xml_logs", "user_gestures")]
-try:
-    live = str2bool(sys.argv[1])
-    if not live:
-        # OFFLINE_I: number of offline tests to run per user and E-level (set to 1 for demo, 10 for fast, 100 for accurate)
-        # NUM_USERS: number of users to test
-        # DATASET: which dataset to use. (use "xml_logs" or "user_gestures")
-        try:
-            OFFLINE_I = int(sys.argv[2])
-        except:
-            print("No command line argument found for OFFLINE_I: defaulting to I = 10.")
-            OFFLINE_I = 10
-
-        try:
-            NUM_USERS = int(sys.argv[3])
-        except:
-            print("No command line argument found for NUM_USERS: defaulting to 1 user.")
-            NUM_USERS = 1
-
-        try:
-            DATASET = str(sys.argv[4])
-        except:
-            print("No command line argument found for DATASET: defaulting to \"xml_logs\".")
-            DATASET = "xml_logs"
-except:
-    print("No command line argument found: defaulting to online mode.")
-    live = True  # True = online mode, False = offline mode.
-
+live = False
+if not live:
+    # offline iteration count
+    OFFLINE_I = 10
+    NUM_USERS = 6
+    DATASET = "numeric"
+    gestures = [
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "zero",
+    ]
 
 if __name__ == "__main__":
     if live:
         canvas = Canvas([False])
         canvas.run()
     else:
-        if DATASET == "xml_logs":
-            gestures = [
-            "triangle",
-            "x",
-            "rectangle",
-            "circle",
-            "check",
-            "caret",
-            "question_mark",
-            "arrow",
-            "left_sq_bracket",
-            "right_sq_bracket",
-            "v",
-            "delete_mark",
-            "left_curly_brace",
-            "right_curly_brace",
-            "star",
-            "pigtail",
-        ]
-        elif DATASET == "user_gestures":
-            gestures = [
-            "arrow",
-            "caret",
-            "check",
-            "circle",
-            "delete",
-            "left_curly_brace",
-            "left_square_bracket",
-            "pigtail",
-            "rectangle",
-            "right_curly_brace",
-            "right_square_bracket",
-            "star",
-            "triangle",
-            "v",
-            "x",
-            "zig-zag",
-        ]
-
         print("Starting loop.")  # loop over data set
 
         path = Path(os.getcwd())
         path = path.parent.absolute()
-        path = str(path) + "\\recognition_logs\\recognition_log.csv"
+        path = str(path) + "\\assets\\recognition_logs\\recognition_log.csv"
         file = open(
             path, "w", newline="", encoding="UTF-8"
         )  # open csv file for logging results
@@ -131,9 +77,7 @@ if __name__ == "__main__":
 
         time_last = 0
         user_accuracies = []  # average recognition percentages for each user
-        user_range = range(2, 2  + NUM_USERS)
-        if DATASET == "user_gestures":
-            user_range = range(1, 1 + NUM_USERS)
+        user_range = range(1, 1 + NUM_USERS)
 
         for user in user_range:
             time_current = time.time()
@@ -196,29 +140,14 @@ if __name__ == "__main__":
                                     gesture + "0" + str(this_candidate)
                                 ]
                             )
-                            '''print("----------")
-                            print(user)
-                            print(gesture + "0" + str(this_candidate))
-                            print(stored_gestures.preprocessed_dataset[user][
-                                    gesture + "0" + str(this_candidate)
-                                ])'''
                         else:
                             candidates[gesture + str(this_candidate)].append(
                                 stored_gestures.preprocessed_dataset[user][
                                     gesture + str(this_candidate)
                                 ]
                             )
-                            '''print("----------")
-                            print(user)
-                            print(gesture + str(this_candidate))
-                            print(stored_gestures.preprocessed_dataset[user][
-                                    gesture + str(this_candidate)
-                                ])'''
 
                     recognizer.clearTrainingSet()
-                    #print("\t\t\tTraining set: " + str(templates))
-                    #print(stored_gestures.preprocessed_dataset[1].keys())
-                    #print(stored_gestures.preprocessed_dataset[1]["arrow03"])
                     recognizer.setOfflineTrainingSet(templates)
 
                     for candidate_name, candidate_points in candidates.items():
